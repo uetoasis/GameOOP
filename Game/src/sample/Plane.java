f8817b74ed1b76ad5dd1960663c2eef714f30df8
ghiaog123 <vuminhhieu2412@gmail.com>
2019-11-04 18:06:50 +0700

    update4

---
+++ Game/src/sample/Plane.java
@@ -0,0 +1,33 @@
+package sample;
+
+import javafx.scene.SnapshotParameters;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+
+public class Plane extends Critter {
+    Image shadowImg;
+
+    @Override
+    void render(GraphicsContext gc ) {
+        SnapshotParameters param = new SnapshotParameters();
+        param.setFill(Color.TRANSPARENT);
+
+        ImageView iv = new ImageView( img );
+        iv.setRotate(30);
+        Image Plane =iv.snapshot(param, null);
+
+        ImageView iv2 = new ImageView( shadowImg );
+        iv2.setRotate(30);
+        Image Shadow = iv2.snapshot( param, null );
+
+        gc.drawImage(Shadow, x + 10, y + 10);
+        gc.drawImage(Plane, x, y);
+    }
+
+    void update() {
+        x += speed + 5;
+        y += speed;
+    }
+}
